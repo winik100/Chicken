e54f632958e494fd74da038b503c26612cb32e15
Tom Freund <tom.freund@uni-duesseldorf.de>
2022-03-18 10:03:37 +0100

    Security Authorisierungstests geschrieben

--- infrastructure/src/main/java/de/hhu/propra/chicken/web/configuration/WebSecurityConfiguration.java
+++ infrastructure/src/main/java/de/hhu/propra/chicken/web/configuration/WebSecurityConfiguration.java
@@ -38,6 +38,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .antMatchers("/").permitAll()
                         .anyRequest().authenticated()
                 )
+                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .logout(l -> l.logoutSuccessUrl("/").permitAll())
                 .oauth2Login();
--- infrastructure/src/test/java/de/hhu/propra/chicken/web/SecurityTest.java
+++ infrastructure/src/test/java/de/hhu/propra/chicken/web/SecurityTest.java
@@ -1,16 +1,23 @@
 package de.hhu.propra.chicken.web;
 
+import de.hhu.propra.chicken.aggregates.BuchungsService;
 import de.hhu.propra.chicken.web.configuration.MethodSecurityConfiguration;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.Collections;
@@ -28,6 +35,9 @@ public class SecurityTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    BuchungsService buchungsService;
+
     OAuth2AuthenticationToken buildPrincipal(String role, String name) {
         Map<String, Object> attributes = new HashMap<>();
         attributes.put("login", name);
@@ -38,63 +48,134 @@ public class SecurityTest {
         return new OAuth2AuthenticationToken(user, authorities, "whatever");
     }
 
-//    @Test
-//    void UnauthorizedWithoutLogin() throws Exception {
-//        mockMvc.perform(get("/admin")).andExpect(status().isUnauthorized());
-//    }
+    @Test
+    void UnauthorizedWithoutLogin() throws Exception {
+        mockMvc.perform(get("/admin")).andExpect(status().isUnauthorized());
+    }
 
-// @Test
-// void userLogin() throws Exception {
-// OAuth2AuthenticationToken principal = buildPrincipal("user", "Max Mustermann");
-// MockHttpSession session = new MockHttpSession();
-// session.setAttribute(
-// HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
-// new SecurityContextImpl(principal));
-//
-// mockMvc.perform(get("/game")
-// .session(session))
-// .andExpect(status().isOk());
-// }
-//
-// @Test
-// void userForbiddenOnAdmin() throws Exception {
-// OAuth2AuthenticationToken principal = buildPrincipal("user", "Max Mustermann");
-// MockHttpSession session = new MockHttpSession();
-// session.setAttribute(
-// HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
-// new SecurityContextImpl(principal));
-//
-// mockMvc.perform(get("/admin")
-// .session(session))
-// .andExpect(status().isOk());
-// }
-//
-// @Test
-// void AdminIsOk() throws Exception {
-// OAuth2AuthenticationToken principal = buildPrincipal("admin", "Max Mustermann");
-// MockHttpSession session = new MockHttpSession();
-// session.setAttribute(
-// HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
-// new SecurityContextImpl(principal));
-//
-// mockMvc.perform(get("/admin")
-// .session(session))
-// .andExpect(status().isOk());
-// }
-//
-// @Test
-// void AdminNameIsWritten() throws Exception {
-// OAuth2AuthenticationToken principal = buildPrincipal("admin", "Ariane");
-// MockHttpSession session = new MockHttpSession();
-// session.setAttribute(
-// HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
-// new SecurityContextImpl(principal));
-//
-// var result = mockMvc.perform(get("/admin")
-// .session(session))
-// .andExpect(model().attribute("user", "Ariane"))
-// .andReturn();
-// assertThat(result.getResponse().getContentAsString()).contains("Ariane");
-// }
+    @Test
+    @DisplayName("User kann auf Startseite zugreifen")
+    void test_2() throws Exception {
+        OAuth2AuthenticationToken principal = buildPrincipal("user", "Max Mustermann");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                new SecurityContextImpl(principal));
 
+        mockMvc.perform(get("/")
+                        .session(session))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("User kann nicht auf Tutorenseite zugreifen")
+    void test_3() throws Exception {
+        OAuth2AuthenticationToken principal = buildPrincipal("user", "Max Mustermann");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                new SecurityContextImpl(principal));
+
+        mockMvc.perform(get("/tutor")
+                        .session(session))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("User kann nicht auf Adminseite zugreifen")
+    void test_4() throws Exception {
+        OAuth2AuthenticationToken principal = buildPrincipal("user", "Max Mustermann");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                new SecurityContextImpl(principal));
+
+        mockMvc.perform(get("/admin")
+                        .session(session))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("Tutor kann auf Startseite zugreifen")
+    void test_5() throws Exception {
+        OAuth2AuthenticationToken principal = buildPrincipal("tutor", "Max Mustermann");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                new SecurityContextImpl(principal));
+
+        mockMvc.perform(get("/")
+                        .session(session))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Tutor kann auf Tutorseite zugreifen")
+    void test_6() throws Exception {
+        OAuth2AuthenticationToken principal = buildPrincipal("tutor", "Max Mustermann");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                new SecurityContextImpl(principal));
+
+        mockMvc.perform(get("/tutor")
+                        .session(session))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Tutor kann nicht auf Adminseite zugreifen")
+    void test_7() throws Exception {
+        OAuth2AuthenticationToken principal = buildPrincipal("tutor", "Max Mustermann");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                new SecurityContextImpl(principal));
+
+        mockMvc.perform(get("/admin")
+                        .session(session))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("Admin kann auf Startseite zugreifen")
+    void test_8() throws Exception {
+        OAuth2AuthenticationToken principal = buildPrincipal("admin", "Max Mustermann");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                new SecurityContextImpl(principal));
+
+        mockMvc.perform(get("/")
+                        .session(session))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Admin kann auf Tutorseite zugreifen")
+    void test_9() throws Exception {
+        OAuth2AuthenticationToken principal = buildPrincipal("admin", "Max Mustermann");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                new SecurityContextImpl(principal));
+
+        mockMvc.perform(get("/tutor")
+                        .session(session))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Admin kann auf Adminseite zugreifen")
+    void test_10() throws Exception {
+        OAuth2AuthenticationToken principal = buildPrincipal("admin", "Max Mustermann");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                new SecurityContextImpl(principal));
+
+        mockMvc.perform(get("/admin")
+                        .session(session))
+                .andExpect(status().isOk());
+    }
 }
