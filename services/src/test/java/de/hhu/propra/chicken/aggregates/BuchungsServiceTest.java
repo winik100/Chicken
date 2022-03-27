31c8b7cfef6cdab44921fcb1b8ba032b030d73c3
charzim <charlotte_zimmer@outlook.de>
2022-03-27 16:19:22 +0200

    CSS angepasst + BuchungsServiceTest Namen gefixt

--- services/src/test/java/de/hhu/propra/chicken/aggregates/BuchungsServiceTest.java
+++ services/src/test/java/de/hhu/propra/chicken/aggregates/BuchungsServiceTest.java
@@ -121,7 +121,7 @@ public class BuchungsServiceTest {
     @Test
     @DisplayName("Beginnt der geplante Urlaub vor Freistellungszeitraum und endet exakt zum Freistellungsbeginn, wird " +
             "das Urlaubsende auf Freistellungsbeginn gesetzt.")
-    void test_6b() {
+    void test_7() {
         LocalDateTime startUrlaub = LocalDateTime.of(2022, 3, 8, 10, 30);
         LocalDateTime endeUrlaub = LocalDateTime.of(2022, 3, 8, 11, 30);
         LocalDateTime startFreistellung = OK_1130_1230.startFreistellungBerechnen();
@@ -136,7 +136,7 @@ public class BuchungsServiceTest {
     @Test
     @DisplayName("Beginnt der geplante Urlaub im Freistellungszeitraum und endet danach, wird der Urlaubsstart " +
             "auf Freistellungsende gesetzt.")
-    void test_7() {
+    void test_8() {
         LocalDateTime startUrlaub = LocalDateTime.of(2022, 3, 8, 11, 30);
         LocalDateTime endeUrlaub = LocalDateTime.of(2022, 3, 8, 13, 0);
         LocalDateTime startFreistellung = OK_1130_1230.startFreistellungBerechnen();
@@ -150,7 +150,7 @@ public class BuchungsServiceTest {
     @Test
     @DisplayName("Wenn keine Konflikte mit Freistellungszeiträumen exisitieren, wird der geplante Urlaub " +
             "unverändert genommen.")
-    void test_8() throws IOException {
+    void test_9() throws IOException {
         when(studentRepo.studentMitGitHubHandle(any())).thenReturn(student);
         
         when(klausurRepo.klausurMitLsfId(any())).thenReturn(OK_12_13);
@@ -178,7 +178,7 @@ public class BuchungsServiceTest {
     @Test
     @DisplayName("Der Student hat keine Klausur am gleichen Tag, aber bereits gebuchten Urlaub. Liegen 90 Minuten " +
             "zwischen dem bestehendem und dem geplanten Urlaub, wird dieser unverändert genommen.")
-    void test_9() throws IOException {
+    void test_10() throws IOException {
         when(student.hatUrlaubAm(any())).thenReturn(true);
         
         when(studentRepo.studentMitGitHubHandle(any())).thenReturn(student);
@@ -210,7 +210,7 @@ public class BuchungsServiceTest {
     @Test
     @DisplayName("Der Student hat keine Klausur am gleichen Tag und keinen bereits gebuchten Urlaub. Ist die Dauer " +
             "des geplanten Urlaubs weniger als 150 Minuten, wird dieser unverändert genommen.")
-    void test_10() throws IOException {
+    void test_11() throws IOException {
         when(studentRepo.studentMitGitHubHandle(any())).thenReturn(student);
         
         when(buchungsValidierung.buchungLiegtNachZeitpunkt(any(), any())).thenReturn(true);
@@ -236,7 +236,7 @@ public class BuchungsServiceTest {
     @Test
     @DisplayName("Hat der Student eine Onlineklausur von 11 bis 12, wird geplanter Urlaub von 09:30 bis 11:30 " +
             "stattdessen von 09:30 bis 10:30 genommen.")
-    void test_11() throws IOException{
+    void test_12() throws IOException{
         when(studentRepo.studentMitGitHubHandle(any())).thenReturn(student);
 
         when(buchungsValidierung.buchungLiegtNachZeitpunkt(any(), any())).thenReturn(true);
@@ -262,7 +262,7 @@ public class BuchungsServiceTest {
 
     @Test
     @DisplayName("Der Student kann eine Klausur anmelden, wenn sie sich mit keiner bereits angemeldeten überschneidet.")
-    void test_12() throws IOException {
+    void test_13() throws IOException {
         when(studentRepo.studentMitGitHubHandle(any())).thenReturn(student);
         when(klausurRepo.klausurMitLsfId(any())).thenReturn(PK_12_13);
         when(buchungsValidierung.buchungLiegtNachZeitpunkt(any(), any())).thenReturn(true);
@@ -278,7 +278,7 @@ public class BuchungsServiceTest {
     @Test
     @DisplayName("Meldet sich der Student für eine Klausur an, die sich mit besthendem Urlaub überschneidet, wird " +
             "dieser so angepasst, dass es keine Überschneidung mehr gibt, und die Klausur wird angemeldet.")
-    void test_13() throws IOException {
+    void test_14() throws IOException {
         when(student.ueberschneidungKlausurMitBestehendemUrlaub(any())).thenReturn(true);
         when(studentRepo.studentMitGitHubHandle(any())).thenReturn(student);
         when(klausurRepo.klausurMitLsfId(any())).thenReturn(PK_12_13);
@@ -295,7 +295,7 @@ public class BuchungsServiceTest {
 
     @Test
     @DisplayName("Ist die Dauer des geplanten Urlaubs kürzer als 15 Minuten, wird dieser nicht genommen.")
-    void test_14() throws IOException {
+    void test_15() throws IOException {
         when(studentRepo.studentMitGitHubHandle(any())).thenReturn(student);
         when(buchungsValidierung.buchungLiegtNachZeitpunkt(any(), any())).thenReturn(true);
         when(buchungsValidierung.liegtImPraktikumsZeitraum(any(), any())).thenReturn(true);
@@ -312,7 +312,7 @@ public class BuchungsServiceTest {
     @Test
     @DisplayName("Beginnt der geplante Urlaub genau zu Freistellungsbeginn und endet innerhalb der Freistellungszeit, " +
             "wird er nicht genommen.")
-    void test_15() throws IOException {
+    void test_16() throws IOException {
         when(studentRepo.studentMitGitHubHandle(any())).thenReturn(student);
 
         when(buchungsValidierung.buchungLiegtNachZeitpunkt(any(), any())).thenReturn(true);
@@ -335,7 +335,7 @@ public class BuchungsServiceTest {
 
     @Test
     @DisplayName("Liegt der geplante Urlaub komplett innerhalb der Freistellungszeit, wird er nicht genommen.")
-    void test_16() throws IOException {
+    void test_17() throws IOException {
         when(studentRepo.studentMitGitHubHandle(any())).thenReturn(student);
 
         when(buchungsValidierung.buchungLiegtNachZeitpunkt(any(), any())).thenReturn(true);
